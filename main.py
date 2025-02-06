984574a3f905d248cae355153692c0d640612a7c
BallSpins <sandalupil@gmail.com>
2025-02-06 08:54:44 +0700

    test

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get('/')
+def root():
+    return {'message': 'message'}
+
 @app.get('/{msg}')
-def root(msg):
+def msg(msg):
     return {'message': msg}
